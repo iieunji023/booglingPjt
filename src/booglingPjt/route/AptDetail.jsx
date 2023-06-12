7ac02e741eef3d3cdc69b7b989db75b881e372d2
BTC-LeeSoonMin <soonmin07@gmail.com>
2023-06-12 19:04:45 +0900

    리액트 구조 생성1

---
+++ src/booglingPjt/route/AptDetail.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AptDetail = () => {
+    return(
+        <>
+        </>
+    );
+}
+export default AptDetail;
---
+++ src/booglingPjt/route/AptPriceRank.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AptPriceRank = () => {
+    return(
+        <>
+        </>
+    );
+}
+export default AptPriceRank;
