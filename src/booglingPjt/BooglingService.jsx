ecb49328e11d337231cc1157474c919d3a816a43
BTC-LeeSoonMin <soonmin07@gmail.com>
2023-06-13 12:43:44 +0900

    제목 : 국토교통부_아파트매매 실거래 상세 자료 API 데이터 수집 본문 :  API 호출 후 Console, 브라우저에 표시 특이사항: 일일트래픽 초과로 실 데이터가 아닌 트래픽 초과 에러 메시지를 출력해보는 것으로 대체, 익일 실 데이터 기준으로 코드 수정 필요.

--- src/booglingPjt/BooglingService.jsx
+++ src/booglingPjt/BooglingService.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Search from "./route/Search";
 
 const BooglingService = () => {
     return(
         <>
         test page.
+        <Search/>
         </>
     );
 }
--- src/booglingPjt/route/Search.jsx
+++ src/booglingPjt/route/Search.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Search = () => {
-    return(
-        <>
-        </>
+    const serviceKey = "IyQg8I2dXbv8kkUs2Gki35cm64Cu%2BxaUWkNCsFipH3WWV6%2FiZD4HHrq4v%2Bykezvft92l9H5S0zULIYrQonfaUA%3D%3D" // 서비스키(필수)
+    const pageNo = 1;          // 페이지 번호(옵션)
+    const numOfRows = 100;     // 한 페이지 결과 수(옵션)
+    const LAWD_CD = 11110;     // 지역코드(필수)
+    const DEAL_YMD = 201512;   // 계약년월(필수)
+    const URL = `http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&LAWD_CD=${LAWD_CD}&DEAL_YMD=${DEAL_YMD}`;
+
+
+    const [data, setData] = useState([]);
+
+    useEffect(()=> {
+        fetch(URL)
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+            var xmlDoc = new DOMParser().parseFromString(data, 'text/xml');
+            var x=xmlDoc.getElementsByTagName("resultMsg");
+            console.log(x[0].textContent);
+            setData(x[0].textContent)
+        
+            return xmlDoc;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, [])
+
+    return (
+
+        <div>
+            <p>{data}</p>
+        </div>
     );
 }
+
+
 export default Search;
