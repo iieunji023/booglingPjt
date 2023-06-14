b350ef1d6a17a9a6e26fea7c95a9c2fb4762fb55
iieunji023 <iieunji023@gmail.com>
2023-06-14 14:43:27 +0900

    첫번째 작업완료

--- src/booglingPjt/BooglingService.jsx
+++ src/booglingPjt/BooglingService.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./route/SignUp";
+import { useState } from "react";
 
 const BooglingService = () => {
-    return(
-        <>
-        test page.
-        </>
-    );
-}
+  const [userDB, setUserDB] = useState(new Map());
+  return (
+    <BrowserRouter>
+      <header />
+      <nav />
+      <Routes>
+        <Route path="/user/sign_up" element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default BooglingService;
