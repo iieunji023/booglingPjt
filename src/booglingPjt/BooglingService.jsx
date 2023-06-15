6d619e962aae9bff88a9dfb82caf9fabb2b2fb82
jinbeom <jinbeom3238@gmail.com>
2023-06-15 10:36:04 +0900

    1

--- src/booglingPjt/BooglingService.jsx
+++ src/booglingPjt/BooglingService.jsx
@@ -1,32 +1,44 @@
-
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./Header";
 import Nav from "./Nav";
-import './css/common.css';
-import './css/index.css';
+import "./css/common.css";
+import "./css/index.css";
 import Main from "./route/Main";
-import axios from 'axios';
+import SignUp from "./route/SignUp";
+import SignUpResult from "./route/SignUpResult";
+import SignIn from "./route/SignIn";
+import SignInResult from "./route/SignInResult";
+import UserModify from "./route/UserModify";
+import UserModifyResult from "./route/UserModifyResult";
 
-const serviceKey = "IyQg8I2dXbv8kkUs2Gki35cm64Cu%2BxaUWkNCsFipH3WWV6%2FiZD4HHrq4v%2Bykezvft92l9H5S0zULIYrQonfaUA%3D%3D" // 서비스키(필수)
-const pageNo = 1;          // 페이지 번호(옵션)
-const numOfRows = 10;     // 한 페이지 결과 수(옵션)
+import axios from "axios";
+
+const serviceKey =
+    "IyQg8I2dXbv8kkUs2Gki35cm64Cu%2BxaUWkNCsFipH3WWV6%2FiZD4HHrq4v%2Bykezvft92l9H5S0zULIYrQonfaUA%3D%3D"; // 서비스키(필수)
+const pageNo = 1; // 페이지 번호(옵션)
+const numOfRows = 10; // 한 페이지 결과 수(옵션)
 
 const BooglingService = () => {
+    const [userDB, setUserDB] = useState(new Map());
+    const signInedMember = useRef("");
+    const changeLoginStatus = useRef("");
 
+    const setLoginedSession = () => {
+        console.log("[MemberService] setLoginedSesstion() CALLED!!");
 
+        changeLoginStatus.current.changeLoginMember(signInedMember);
+    };
 
     // const [lawdCd, setLawdCd] = useState('');
     // const [dealYMD, setdealYMD] = useState('');
-    const [item, setItem] = useState([]);       //중복값이 있는 아파트명 목록
+    const [item, setItem] = useState([]); //중복값이 있는 아파트명 목록
 
     useEffect(() => {
-        console.log('useEffect!!');
+        console.log("useEffect!!");
 
         getRemoteData();
-
-
     }, []);
 
     // let LAWD_CD = `26${lawdCd}0`;           // 지역코드(필수)
@@ -42,24 +54,23 @@ const BooglingService = () => {
     // Handler END
 
     async function getData(y, m, r) {
-        console.log('[Search] getData() CALLED!!');
+        console.log("[Search] getData() CALLED!!");
 
         try {
-
-            let deal_ymd = (y + m) * 1 // 년도월, ex) 2022년 01월 == 202201
-            let region = `26${r}0`
+            let deal_ymd = (y + m) * 1; // 년도월, ex) 2022년 01월 == 202201
+            let region = `26${r}0`;
 
             let url = `http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&LAWD_CD=${region}&DEAL_YMD=${deal_ymd}`;
             const response = await axios.get(url);
             // console.log('response ---> \n', response.data.response.body.items.item);
 
             let items = response.data.response.body.items.item;
-            item.push(items)
+            item.push(items);
             // console.log(items)
             // setItem([...item,items])
             // let temp = item.slice()
             // setItem(temp)
-            console.log('item ---> \n', item)
+            console.log("item ---> \n", item);
 
             // items.map((item, idx) => {
             //     console.log('idx  ---> ', idx);
@@ -68,59 +79,89 @@ const BooglingService = () => {
             //     console.log('item ---> ', item.도로명);
             //     console.log('item ---> ', item.도로명건물본번호코드);
             // });
-
         } catch (error) {
             console.log(error);
-
         } finally {
-            console.log('finally');
-
+            console.log("finally");
         }
-
     }
 
     async function getRemoteData() {
-        console.log('[Search] getRemoteData() CALLED!!');
+        console.log("[Search] getRemoteData() CALLED!!");
 
-        let year = ['2022', '2023'];
-        let month = ['01'];
-        let region = ['11', '14', '17', '20', '23', '26', '29', '32', '35', '38', '41', '44', '47', '50', '53', '71']
-
+        let year = ["2022", "2023"];
+        let month = ["01"];
+        let region = [
+            "11",
+            "14",
+            "17",
+            "20",
+            "23",
+            "26",
+            "29",
+            "32",
+            "35",
+            "38",
+            "41",
+            "44",
+            "47",
+            "50",
+            "53",
+            "71",
+        ];
 
         try {
             year.map(function (y) {
                 month.map(function (m) {
                     region.map(function (r) {
                         getData(y, m, r);
-
-                    })
-
+                    });
                 });
             });
-
         } catch (error) {
             console.log(error);
-
         } finally {
-            console.log('finally');
-
+            console.log("finally");
         }
-
     }
 
     return (
-
         <>
-
             <BrowserRouter>
                 <Header />
                 <Nav />
                 <Routes>
                     <Route path="/" element={<Main />}></Route>
+                    <Route
+                        path="/user/sign_up"
+                        element={<SignUp userDB={userDB} />}
+                    ></Route>
+                    <Route path="/user/sign_up_result" element={<SignUpResult />}></Route>
+                    <Route
+                        path="/user/sign_in"
+                        element={
+                            <SignIn
+                                userDB={userDB}
+                                signInedMember={signInedMember}
+                                setLoginedSession={setLoginedSession}
+                            />
+                        }
+                    ></Route>
+                    <Route path="/user/sign_in_result" element={<SignInResult />}></Route>
+                    <Route
+                        path="/user/modify"
+                        element={
+                            <UserModify userDB={userDB} signInedMember={signInedMember} />
+                        }
+                    ></Route>
+                    <Route
+                        path="/user/modify_result"
+                        element={<UserModifyResult />}
+                    ></Route>
+                    <Route path="*" element={<Main />}></Route>
                 </Routes>
             </BrowserRouter>
-
         </>
     );
-}
+};
 export default BooglingService;
--- src/booglingPjt/Nav.jsx
+++ src/booglingPjt/Nav.jsx
@@ -1,37 +1,92 @@
-import React from "react";
+import React, {
+    forwardRef,
+    useEffect,
+    useImperativeHandle,
+    useState,
+} from "react";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = forwardRef(({ signInedMember, userDB }, ref) => {
+    const [loginedMember, setLoginedMember] = useState("");
+
+    useEffect(() => {
+        console.log("[Nav] useEffect() CALLED!!");
+    });
+
+    const changeLoginMember = (signInedMember) => {
+        console.log("[Nav] changeLoginMember() CALLED!!");
+        console.log("[Nav] signInedMember.current", signInedMember.current);
+
+        setLoginedMember(signInedMember.current);
+    };
+
+    useImperativeHandle(ref, () => ({
+        changeLoginMember,
+    }));
+
+    const signOutClickHandler = () => {
+        console.log("[Nav] signOutClickHandler() CALLED!!");
+
+        signInedMember.current = "";
+        setLoginedMember("");
+    };
+
+    const modifyAccountClickHandler = () => {
+        console.log("[Nav] modifyAccountClickHandler() CALLED!!");
+    };
+
     return (
         <nav>
             <div className="bar">
                 <ul className="bar_ul">
+                    <li className="sign_up">
+                        {loginedMember === "" ? (
+                            <Link to="/user/sign_Up">회원가입</Link>
+                        ) : (
+                            <span>[Welcom {loginedMember}]</span>
+                        )}
+                    </li>
+                    <li className="sign_in">
+                        {loginedMember === "" ? (
+                            <Link to="/user/sign_in">로그인</Link>
+                        ) : (
+                            <Link to="/" onClick={signOutClickHandler}>
+                                로그아웃
+                            </Link>
+                        )}
+                    </li>
 
-                    <li className="sign_in"><Link>로그인</Link></li>
-                    <li className="sign_up"><Link>회원가입</Link></li>
-                    <li className="modify"><Link>회원정보수정</Link></li>
+                    <li className="modify">
+                        {loginedMember === "" ? null : (
+                            <Link to="/user/modify" onClick={modifyAccountClickHandler}>
+                                회원정보수정
+                            </Link>
+                        )}
+                    </li>
 
-                    <li className="btn"><Link>찾기</Link></li>
+                    <li className="btn">
+                        <Link>찾기</Link>
+                    </li>
 
                     <li className="region_settings">
                         <span>지역설정</span>
                         <select name="region">
-                            <option value='11'>중구</option>
-                            <option value='14'>서구</option>
-                            <option value='17'>동구</option>
-                            <option value='20'>영도구</option>
-                            <option value='23'>부산진구</option>
-                            <option value='26'>동래구</option>
-                            <option value='29'>남구</option>
-                            <option value='32'>북구</option>
-                            <option value='35'>해운대구</option>
-                            <option value='38'>사하구</option>
-                            <option value='41'>금정구</option>
-                            <option value='44'>강서구</option>
-                            <option value='47'>연제구</option>
-                            <option value='50'>수영구</option>
-                            <option value='53'>사상구</option>
-                            <option value='71'>기장군</option>
+                            <option>중구</option>
+                            <option>서구</option>
+                            <option>동구</option>
+                            <option>영도구</option>
+                            <option>부산진구</option>
+                            <option>동래구</option>
+                            <option>남구</option>
+                            <option>북구</option>
+                            <option>해운대구</option>
+                            <option>사하구</option>
+                            <option>금정구</option>
+                            <option>강서구</option>
+                            <option>연제구</option>
+                            <option>수영구</option>
+                            <option>사상구</option>
+                            <option>기장군</option>
                         </select>
                     </li>
 
@@ -43,5 +98,5 @@ const Nav = () => {
             </div>
         </nav>
     );
-}
+});
 export default Nav;
